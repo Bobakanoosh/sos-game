b73c4897f1a639b05c79060d7b4c152f66827501
Bobakanoosh <jack.bobakanoosh@gmail.com>
2023-04-21 20:32:29 -0500

    Add vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import devtools from "solid-devtools/vite";
+import vercelAdapter from "solid-start-vercel";
 
 export default defineConfig({
 	plugins: [
-		solid(),
+		solid({
+			adapter: vercelAdapter({ edge: true }),
+		}),
 		devtools({
 			autoname: true,
 		}),
