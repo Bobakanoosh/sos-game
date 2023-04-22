6639391661cde52606239f6913754e43e21f3029
Bobakanoosh <jack.bobakanoosh@gmail.com>
2023-04-21 20:36:39 -0500

    Revert "Use cloudflare pages adapter instead"
    This reverts commit 35bf7306e26e0e81c0cb6ec758d2502c777866fa.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import devtools from "solid-devtools/vite";
-import cloudflareAdapter from "solid-start-cloudflare-pages";
+import vercelAdapter from "solid-start-vercel";
 
 export default defineConfig({
 	plugins: [
 		solid({
-			adapter: cloudflareAdapter({ edge: true }),
+			adapter: vercelAdapter({ edge: true }),
 		}),
 		devtools({
 			autoname: true,
